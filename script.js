4cb80a24f529cd6e1489f04d466d891e3f303ac0
shimal007 <shimalakmald.23aim@kongu.edu>
2025-01-08 22:19:23 +0530

    pushing

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+    document.getElementById('sidebar').style.width = '250px';
+});
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('sidebar').style.width = '0';
+});
