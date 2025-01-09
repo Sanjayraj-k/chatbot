e2aaecf7348594bb90c36c476a381e8768e69018
shimal007 <shimalakmald.23aim@kongu.edu>
2025-01-09 14:39:11 +0530

    sanjayleader

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.getElementById('menuButton').addEventListener('click', function() {
-    document.getElementById('sidebar').style.width = '250px';
-});
-
-document.getElementById('closeButton').addEventListener('click', function() {
-    document.getElementById('sidebar').style.width = '0';
+document.getElementById('menuButton').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    // Toggle the width of the sidebar
+    if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0'; // Close the sidebar
+    } else {
+        sidebar.style.width = '250px'; // Open the sidebar
+    }
 });
